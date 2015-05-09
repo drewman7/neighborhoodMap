a03d0fb287f1eaa593c686c67105ceacaa9f526e
Drew <drewstuff5@hotmail.com>
2015-05-09 11:26:26 -0400

    locMarker

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 var modelData = {
   title: "Neighborhood MAP!",
   geocoder: {},
-  map: {}
+  map: {},
+  infowindow: {},
+  address: "",
+  markerType: ""
 }
 
 //var geocoder;
@@ -17,7 +20,7 @@ var viewModel = function() {
     modelData.geocoder = new google.maps.Geocoder();
     var mapOptions = {
       center: { lat: 36.26, lng: -95.147},
-      zoom: 2
+      zoom: 3
     };
     map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
@@ -26,7 +29,7 @@ var viewModel = function() {
   google.maps.event.addDomListener(window, 'load', self.mapInitialize);
 
   this.codeAddress = function() {
-    var address = document.getElementById("address").value;
+    address = document.getElementById("address").value;
     modelData.geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         //map.setCenter(results[0].geometry.location);
@@ -48,18 +51,44 @@ var viewModel = function() {
     });
   };
 
+  this.locMarker = function() {
+    markerType = document.getElementById("markerType").value;
+    var request = {
+      location: address,
+      radius: 1000,
+      types: ['store']
+    };
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, self.callback);
+  };
+
+  this.callback = function(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        self.createMarker(results[i]);
+      }
+    }
+  }:
+
+  this.createMarker = function(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+    });
+  };
+
+
   this.codeMarker = function() {
     var markerAddress = document.getElementById("markerAddress").value;
     modelData.geocoder.geocode( { 'address': markerAddress}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
-        //map.setCenter(results[0].geometry.location);
-        //var mapOptions = {
-        //  center: results[0].geometry.location,
-        //  zoom: 10
-        //};
-        //var map = new google.maps.Map(document.getElementById('map-canvas'),
-        //    mapOptions);  
-
         var marker = new google.maps.Marker({
             map: map,
             position: results[0].geometry.location,
