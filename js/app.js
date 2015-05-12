8c77b81375ab71373e55184e36817491b459b7fb
Drew <drewstuff5@hotmail.com>
2015-05-11 20:52:20 -0400

    filter

--- js/app.js
+++ js/app.js
@@ -117,7 +117,7 @@ var viewModel = function() {
       console.log(self.filter());
       if (!self.filter()) {
         for (var i = 0; i < self.markerListArray2().length; i++) {
-          self.markerListArray2()[i].marker.setMap(map);
+          //self.markerListArray2()[i].marker.setMap(map);
         };
         return self.markerListArray2();
       } else {
@@ -126,7 +126,7 @@ var viewModel = function() {
         for (var i = 0; i < self.markerListArray2().length; i++) {
           if (self.markerListArray2()[i].name.toLowerCase().search(self.filter().toLowerCase()) !== -1) {
             temp.push(self.markerListArray2()[i]);
-            self.markerListArray2()[i].marker.setMap(map);
+            //self.markerListArray2()[i].marker.setMap(map);
           } else {
             self.markerListArray2()[i].marker.setMap(null);
           };
