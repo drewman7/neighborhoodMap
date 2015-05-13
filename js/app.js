032b71beb13f717a7e337821450c4c8822565f14
Drew <drewstuff5@hotmail.com>
2015-05-13 08:02:56 -0400

    click

--- js/app.js
+++ js/app.js
@@ -110,9 +110,9 @@ var viewModel = function() {
     //self.markerListArray2()[index].marker = marker;
 
     google.maps.event.addListener(marker, 'click', function() {
-      self.infoWindowAppear(index, place, marker);
-      //infowindow.setContent(place.name);
-      //infowindow.open(map, this);
+      //self.infoWindowAppear(index, place, marker);
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
     });
   };
 
@@ -143,16 +143,11 @@ var viewModel = function() {
       };
   }, this);
 
-  this.infoWindowAppear = function(listIndex, data, marker) {
+  this.infoWindowAppear = function(listIndex, data) {
    
-    console.log(data);
-    //console.log(listIndex());
+
     infowindow.setContent(data.name);
-    if (marker === null) {
-      infowindow.open(map, data.marker);
-    } else {
-      infowindow.open(map, marker);
-    };
+    infowindow.open(map, data.marker);
   };
 
 
