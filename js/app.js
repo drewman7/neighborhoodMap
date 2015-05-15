2f316e5b4add50d10afc7657d1c8c1db7a684633
Drew <drewstuff5@hotmail.com>
2015-05-15 18:18:21 -0400

    wiki

--- js/app.js
+++ js/app.js
@@ -64,6 +64,7 @@ var viewModel = function() {
         alert("Geocode was not successful for the following reason: " + status);
       };
     });
+    self.locMarkerClear();
     self.articles(address);
   };
 
